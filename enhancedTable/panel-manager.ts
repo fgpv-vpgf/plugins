5aaf1f3e2939d4d563d515af3116654791249479
Vellanki <Shruti.Vellanki@ec.gc.ca>
2019-05-16 15:49:28 -0400

    fix(enhancedTable): fix headers disappearing in IE

--- enhancedTable/panel-manager.ts
+++ enhancedTable/panel-manager.ts
@@ -72,12 +72,9 @@ export class PanelManager {
         const enhancedTable = this.panel._element;
         const that = this;
 
-        enhancedTable.mouseover(() => {
-            $('.ag-body-viewport')[0].style.position = 'initial';
-        })
-
-        enhancedTable.mouseleave(() => {
-            $('.ag-body-viewport')[0].style.position = 'initial';
+        $(document).mousemove(() => {
+            $('.ag-body-viewport')[0].style.overflowX = 'auto';
+            $('.ag-body-viewport')[0].style.position = 'static';
         })
 
         enhancedTable.on('keydown keyup', (event) => {
@@ -87,6 +84,8 @@ export class PanelManager {
             const focusedCell = that.tableOptions.api.getFocusedCell();
 
             if (focusedList !== undefined && $(focusedList).hasClass('ag-body-container')) {
+                $('.ag-body-viewport')[0].style.overflowX = 'hidden';
+
                 // if focused on grid body
                 if (event.keyCode === 9 && focusedCell === null) {
                     // if first tab into grid body automatically focus on first cell
@@ -102,7 +101,7 @@ export class PanelManager {
             if ((event.keyCode !== 9 && event.keyCode !== 27) || ($('.element-focused')[0] === undefined && inList)) {
                 // if you are not tabbing or you are tabbing within a list or you're not pressing the escape key
                 // set body to be scrollable
-                $('.ag-body-viewport')[0].style.position = 'initial';
+                $('.ag-body-viewport')[0].style.position = 'static';
             } else {
                 // if you are tabbing between lists, body should be absolute
                 $('.ag-body-viewport')[0].style.position = 'absolute';
@@ -331,7 +330,7 @@ export class PanelManager {
         columns = columns ? columns : this.tableOptions.columnApi.getAllColumns();
         this.tableOptions.columnApi.autoSizeColumns(columns);
         columns.forEach(c => {
-        if (c.actualWidth > maxWidth) {
+            if (c.actualWidth > maxWidth) {
                 this.tableOptions.columnApi.setColumnWidth(c, maxWidth);
             }
         });
